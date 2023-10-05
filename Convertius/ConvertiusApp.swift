600e62fd275829087fb2c2ad491412505d9ad460
Marius <mariusae@me.com>
2023-10-05 19:48:03 +0400

    Initial Commit

---
+++ Convertius/ConvertiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertiusApp.swift
+//  Convertius
+//
+//  Created by Marius Malyshev on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConvertiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
